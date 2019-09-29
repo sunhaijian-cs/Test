57224a5eaea5ff5864e54c12f00e2ea12ffabc32
shj <sunhaijian@XHZY>
2019-09-29 15:32:37 +0800

    v

---
+++ src/java/Test.java
@@ -0,0 +1,16 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @Author: sunhaijian
+ * @Date: 2019/9/29
+ * @Description:
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+        JSONObject jsonObject=new JSONObject();
+        System.out.println(jsonObject.toJSONString());
+    }
+}
